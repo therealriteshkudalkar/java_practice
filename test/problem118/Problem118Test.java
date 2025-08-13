aa605037c2d6769deff788e3caa7157aac465416
Ritesh Kudalkar <therealriteshkudalkar@gmail.com>
2025-08-13 21:06:01 +0530

    Modify test files

--- test/problem118/Problem118Test.java
+++ test/problem118/Problem118Test.java
@@ -1,7 +1,6 @@
 package problem118;
 
 import org.junit.jupiter.api.Test;
-import problem118.Problem118;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,19 +8,19 @@ class Problem118Test {
 
     @Test
     void hasAllCodes() {
-        Problem118 problem117 = new Problem118();
+        Problem118 problem118 = new Problem118();
 
         // Test 1
-        assertTrue(problem117.hasAllCodes("00110110", 2));
+        assertTrue(problem118.hasAllCodes("00110110", 2));
 
         // Test 2
-        assertTrue(problem117.hasAllCodes("0110", 1));
+        assertTrue(problem118.hasAllCodes("0110", 1));
 
         // Test 3
-        assertFalse(problem117.hasAllCodes("0110", 2));
+        assertFalse(problem118.hasAllCodes("0110", 2));
 
         // Test 4
-        assertTrue(problem117.hasAllCodes("00110", 2));
+        assertTrue(problem118.hasAllCodes("00110", 2));
 
     }
 }
